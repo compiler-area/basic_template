32a3c08391243d4d28136c0e1f28c640a8da6e2d
compiler-area <area.compiler@gmail.com>
2020-08-08 16:22:00 +0200

    template first time

---
+++ assets/js/libs/main/json.js
@@ -0,0 +1,6 @@
+$(document).ready(function()
+{
+        $.getJSON("https://jsoneditoronline.org/#left=cloud.75c393edbac241bd9c5377b2f0da4983" , function (data) {
+          console.log(data);
+        })
+});
